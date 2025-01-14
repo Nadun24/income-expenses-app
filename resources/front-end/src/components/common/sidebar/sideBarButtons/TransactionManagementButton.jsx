449f0ba6b08d4220f02c43016e2bd445a2ce07ab
Nadun Sameera Bandara <bandarans2000@gmail.com>
2025-01-14 06:45:58 +0530

    create form filed and drop down colapsed

--- resources/front-end/src/components/common/sidebar/sideBarButtons/TransactionManagementButton.jsx
+++ resources/front-end/src/components/common/sidebar/sideBarButtons/TransactionManagementButton.jsx
@@ -1,10 +1,23 @@
 import {Link} from "react-router";
+import {useState} from "react";
 
 const TransactionManagementButton = () => {
+
+    const [isDropDownOpen,setIsDropDownOpen] = useState(false)
+
+    const handleDropDownOpenAndClose = (isOpen) =>{
+        if(!isOpen){
+            setIsDropDownOpen(true)
+            return
+        }
+        setIsDropDownOpen(false)
+    }
     return (
         <div>
             <li>
-                <a href="#"
+                <Link
+                    to="/add-incomes"
+                    onClick={()=> handleDropDownOpenAndClose(isDropDownOpen)}
                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <svg
                         className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -15,8 +28,8 @@ const TransactionManagementButton = () => {
                     </svg>
                     <span className="flex-1 ms-3 whitespace-nowrap">Transaction Management </span>
 
-                </a>
-                <ul className="py-2 space-y-3 ">
+                </Link>
+                <ul className={isDropDownOpen? "py-2 space-y-3" : "hidden"}>
                     <li className='ml-12'>
                        <Link to="/add-incomes">Add Incomes</Link>
                     </li>
