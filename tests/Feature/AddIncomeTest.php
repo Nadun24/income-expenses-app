5544f20efc335b5556a2908320c25ec60f600736
Nadun Sameera Bandara <bandarans2000@gmail.com>
2025-01-14 11:02:39 +0530

    create test and production database

---
+++ tests/Feature/AddIncomeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use Tests\TestCase;
+
+// final class can not create child code
+final class AddIncomeTest extends TestCase
+{
+    public function test_income_and_category_database_save()
+    {
+        //A - Arrange
+        // dummy data that we need to do the test
+
+
+        //A - Action
+
+        //A - Assertion
+    }
+
+    public function  test_return_bad_response_when_income_exist()
+    {
+
+    }
+}
