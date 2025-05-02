1c99cac7d96b4e5c591636eb2d8a2f1420093346
Nadun Sameera Bandara <bandarans2000@gmail.com>
2025-05-02 23:17:24 +0530

    change get income detrails end point

--- resources/front-end/src/components/common/dashboard/dashboardComponents/transactionManagement/Income/AddIncome.jsx
+++ resources/front-end/src/components/common/dashboard/dashboardComponents/transactionManagement/Income/AddIncome.jsx
@@ -1,73 +1,74 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { addNewIncome } from "../../../../../../utilities/api/AddIncomeApi/IncomeApi.jsx";
-import FormSuccessAlert from "../../../../alert/FormSuccessAlert.jsx";
-import AddNewIncome from "./AddNewIncome.jsx";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { addNewIncome } from '../../../../../../utilities/api/AddIncomeApi/IncomeApi.jsx'
+import FormSuccessAlert from '../../../../alert/FormSuccessAlert.jsx'
+import AddNewIncome from './AddNewIncome.jsx'
 
 const AddIncome = () => {
     const [incomeDetails, setIncomeDetails] = useState({
         income_amount: 0,
-        income_category: "",
-    });
+        income_category: ''
+    })
 
-    const [message, setMessage] = useState("");
-    const [isIncomeAmountFieldEmpty, setIncomeAmountFieldEmpty] = useState(false);
-    const [isIncomeCategoryEmpty, setIsIncomeCategoryEmpty] = useState(false);
-    const [isModalOpen, setIsModelOpen] = useState(false);
+    const [message, setMessage] = useState('')
+    const [isIncomeAmountFieldEmpty, setIncomeAmountFieldEmpty] =
+        useState(false)
+    const [isIncomeCategoryEmpty, setIsIncomeCategoryEmpty] = useState(false)
+    const [isModalOpen, setIsModelOpen] = useState(false)
 
-    const handleIncomeDetailsChange = (event) => {
+    const handleIncomeDetailsChange = event => {
         //destructuring
-        const { name, value } = event.target;
+        const { name, value } = event.target
 
-        setIsIncomeCategoryEmpty(false);
-        setIncomeAmountFieldEmpty(false);
+        setIsIncomeCategoryEmpty(false)
+        setIncomeAmountFieldEmpty(false)
 
-        setIncomeDetails((prevState) => ({ ...prevState, [name]: value }));
-    };
+        setIncomeDetails(prevState => ({ ...prevState, [name]: value }))
+    }
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+    const handleSubmit = async event => {
+        event.preventDefault()
 
         if (!incomeDetails.income_amount) {
-            setIncomeAmountFieldEmpty(true);
-            return;
+            setIncomeAmountFieldEmpty(true)
+            return
         }
         if (!incomeDetails.income_category) {
-            setIsIncomeCategoryEmpty(true);
-            return;
+            setIsIncomeCategoryEmpty(true)
+            return
         }
-        const response = await addNewIncome(incomeDetails);
+        const response = await addNewIncome(incomeDetails)
 
-        const { message } = response.data;
-        setMessage(message);
-    };
+        const { message } = response.data
+        setMessage(message)
+        console.log(message)
+    }
 
-    const handleModalOpenAndClose = (isOpen) => {
-
+    const handleModalOpenAndClose = () => {
         // if (!isOpen) {
         //     setIsModelOpen(false)
         // }else {
         //     setIsModelOpen(true)
         // }
-        setIsModelOpen((prevState) => !prevState )
-
-    };
+        setIsModelOpen(prevState => !prevState)
+    }
 
     return (
         <div>
-            <div className="p-4 sm:ml-64">
-                <div className="p-4 ">
-                    <div className="grid  gap-4 mb-4">
-                        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+            <div className='p-4 sm:ml-64'>
+                <div className='p-4 '>
+                    <div className='grid  gap-4 mb-4'>
+                        <div className='flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800'>
                             <button
-                                type="button"
+                                type='button'
                                 onClick={() => handleModalOpenAndClose()}
-                                className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800 bg-black"
+                                className='inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800 bg-black'
                             >
                                 Create New Income
                             </button>
                         </div>
                     </div>
+
                     <AddNewIncome
                         isModalOpen={isModalOpen}
                         handleModalOpenAndClose={handleModalOpenAndClose}
@@ -76,15 +77,12 @@ const AddIncome = () => {
                         handleIncomeDetailsChange={handleIncomeDetailsChange}
                         isIncomeAmountFieldEmpty={isIncomeAmountFieldEmpty}
                         isIncomeCategoryEmpty={isIncomeCategoryEmpty}
-
                     />
-
-
-
                 </div>
+                <div>table</div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default AddIncome;
+export default AddIncome
---
+++ resources/front-end/src/components/common/dashboard/dashboardComponents/transactionManagement/Income/IncomeListTable.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function IncomeListTable () {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Income Amount</th>
+                        <th>Income Category</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    )
+}
